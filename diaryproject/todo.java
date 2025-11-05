887cf556b34e8bcc8a333e688f53bf8e1dd0c11e
kimkihwan <rlghks1229@naver.com>
2025-11-05 12:38:36 +0900

    Update todo.java

--- diaryproject/todo.java
+++ diaryproject/todo.java
@@ -2,8 +2,15 @@ package diaryproject;
 
 public class todo {
 
-	public todo() {
-		// TODO Auto-generated constructor stub
-	}
+	 public static void main(String[] args) {
+        int a = 10, b = 0;
+        try {
+            int r = a / b;
+            System.out.println("결과: " + r);
+        } catch (ArithmeticException e) {
+            System.out.println("🚨 0으로 나눌 수 없습니다: " + e.getMessage());
+        } finally {
+            System.out.println("프로그램 종료");
+        }}
 
 }
