7af8a3051935a83c08ecaa9ffe72a5ad02213a05
kimkihwan <rlghks1229@naver.com>
2025-11-05 12:39:14 +0900

    dd

--- diaryproject/todo.java
+++ diaryproject/todo.java
@@ -2,15 +2,6 @@ package diaryproject;
 
 public class todo {
 
-	 public static void main(String[] args) {
-        int a = 10, b = 0;
-        try {
-            int r = a / b;
-            System.out.println("결과: " + r);
-        } catch (ArithmeticException e) {
-            System.out.println("🚨 0으로 나눌 수 없습니다: " + e.getMessage());
-        } finally {
-            System.out.println("프로그램 종료");
-        }}
+	 
 
 }
